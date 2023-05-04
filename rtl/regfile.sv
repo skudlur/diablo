cb4f6d8c1db552b7fcc32ccc1d0968ddfafbd572
Suhas KV <suhaskv0111@gmail.com>
2023-05-04 17:10:08 +0530

    Fix: Fixed regfile reg size, parameterized xlen

--- rtl/regfile.sv
+++ rtl/regfile.sv
@@ -3,21 +3,21 @@
 */
 
 
-module int_regfile ( input logic clk,
+module int_regfile #parameter(XLEN=64) ( input logic clk,
 		       input logic rst,
 		       
 		       input logic [4:0] raddr_in1,
 		       input logic [4:0] raddr_in2,
 		       input logic [4:0] waddr_in,
-		       input logic [31:0] wdata_in,
+		       input logic [XLEN-1:0] wdata_in,
 		       input logic wen_in,
 
-		       output logic [31:0] rdata_out1,
-		       output logic [31:0] rdata_out2,
+		       output logic [XLEN-1:0] rdata_out1,
+		       output logic [XLEN-1:0] rdata_out2,
 );
-	reg [31:0] x0 = 32'b0, x1, x2, x3, x4, x5, x6, x7, x8, x9;
-	reg [31:0] x10, x11, x12, x13, x14, x15, x16, x17, x18, x19;
-	reg [31:0] x20, x21, x22, x23, x24, x25, x26, x27, x28, x29, x30, x31;
+	reg [XLEN-1:0] x0 = 0, x1, x2, x3, x4, x5, x6, x7, x8, x9;
+	reg [XLEN-1:0] x10, x11, x12, x13, x14, x15, x16, x17, x18, x19;
+	reg [XLEN-1:0] x20, x21, x22, x23, x24, x25, x26, x27, x28, x29, x30, x31;
 
 	logic write_fault = 1'b0;
 	
@@ -99,37 +99,37 @@ module int_regfile ( input logic clk,
 
 	always @(posedge clk) begin
 		if (rst == 1) begin
-			x1 <= 32'b0;
-			x2 <= 32'b0;
-			x3 <= 32'b0;
-			x4 <= 32'b0;
-			x5 <= 32'b0;
-			x6 <= 32'b0;
-			x7 <= 32'b0;
-			x8 <= 32'b0;
-			x9 <= 32'b0;
-			x10 <= 32'b0;
-			x11 <= 32'b0;
-			x12 <= 32'b0;
-			x13 <= 32'b0;
-			x14 <= 32'b0;
-			x15 <= 32'b0;
-			x16 <= 32'b0;
-			x17 <= 32'b0;
-			x18 <= 32'b0;
-			x19 <= 32'b0;
-			x20 <= 32'b0;
-			x21 <= 32'b0;
-			x22 <= 32'b0;
-			x23 <= 32'b0;
-			x24 <= 32'b0;
-			x25 <= 32'b0;
-			x26 <= 32'b0;
-			x27 <= 32'b0;
-			x28 <= 32'b0;
-			x29 <= 32'b0;
-			x30 <= 32'b0;
-			x31 <= 32'b0;
+			x1 <= 0;
+			x2 <= 0;
+			x3 <= 0;
+			x4 <= 0;
+			x5 <= 0;
+			x6 <= 0;
+			x7 <= 0;
+			x8 <= 0;
+			x9 <= 0;
+			x10 <= 0;
+			x11 <= 0;
+			x12 <= 0;
+			x13 <= 0;
+			x14 <= 0;
+			x15 <= 0;
+			x16 <= 0;
+			x17 <= 0;
+			x18 <= 0;
+			x19 <= 0;
+			x20 <= 0;
+			x21 <= 0;
+			x22 <= 0;
+			x23 <= 0;
+			x24 <= 0;
+			x25 <= 0;
+			x26 <= 0;
+			x27 <= 0;
+			x28 <= 0;
+			x29 <= 0;
+			x30 <= 0;
+			x31 <= 0;
 		end
 		else if (wen_in) begin
 			case (waddr_in)
