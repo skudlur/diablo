ca486ad38ef06a8fa73a6a42e01fb41a79dbad09
Joyen Benitto <75515758+JoyenBenitto@users.noreply.github.com>
2023-05-04 09:58:07 +0530

    Adding Instruction_Memory
    Not Verified its a DUT

---
+++ rtl/Instruction_Memory.sv
@@ -0,0 +1,15 @@
+module Instruction_Memory #(parameter N=6,M=64)
+(input logic clk,
+input logic we,
+input logic[N-1:0]adr,
+input logic[M-1:0]din,
+output logic[M-1:0]dout);
+
+logic[M-1:0]mem[2**N-1:0];
+
+always_ff@(posedge clk)
+    if(we) mem[adr] <= din;
+    
+    assign dout = mem[adr];
+    
+endmodule
