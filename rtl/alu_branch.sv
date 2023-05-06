f2c500bd4e804e663982248e2fc943c8f7538770
Aakarsh Vinay <102870828+akarxxx1030@users.noreply.github.com>
2023-05-06 10:51:33 +0530

    Update alu_branch.sv

--- rtl/alu_branch.sv
+++ rtl/alu_branch.sv
@@ -1,7 +1,7 @@
 module alu_branch(pccurrent,pcoffset,updatedpc);
-input logic [63:0]pccurrent;
-input logic [63:0]pcoffset;
-output logic [63:0]updatedpc;
+input logic [63:0] pccurrent;
+input logic [63:0] pcoffset;
+output logic [63:0] updatedpc;
 assign updatedpc = pccurrent + pcoffset;
 endmodule
 
