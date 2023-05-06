5d065ac2b30c31b9ead157e47ba0e53ef2a4924a
Aakarsh Vinay <102870828+akarxxx1030@users.noreply.github.com>
2023-05-06 10:35:03 +0530

    Update alu_control.sv

--- rtl/alu_control.sv
+++ rtl/alu_control.sv
@@ -6,16 +6,16 @@ input logic  funct7; // funct7[30]
 input logic  [N-2:0]funct3; //funct3[14:12]
 output logic [N-1:0]aluoperation; //ALUoperation fed into the ALU Unit
 always @ (*) //senses any input
-begin
-casex({funct7,funct3,aluop}) //incorporates don't cares
-6'bxxxx00 : aluoperation = 4'b0010; //Addition wrt Load/Store
-6'bxxxx01 : aluoperation = 4'b0110; //Subtraction wrt Branch
-6'b000010 : aluoperation = 4'b0010; //Addition
-6'b100010 : aluoperation = 4'b0110; //Subtraction
-6'b011110 : aluoperation = 4'b0000; //AND-ing
-6'b011010 : aluoperation = 4'b0001; //OR-ing
-endcase
-end
+   begin
+        casex({funct7,funct3,aluop}) //incorporates don't cares
+           6'bxxxx00 : aluoperation = 4'b0010; //Addition wrt Load/Store
+           6'bxxxx01 : aluoperation = 4'b0110; //Subtraction wrt Branch
+           6'b000010 : aluoperation = 4'b0010; //Addition
+           6'b100010 : aluoperation = 4'b0110; //Subtraction
+           6'b011110 : aluoperation = 4'b0000; //AND-ing
+           6'b011010 : aluoperation = 4'b0001; //OR-ing
+        endcase
+   end
 endmodule
 
 
