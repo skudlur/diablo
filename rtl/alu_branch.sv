f5913a8de9f01eb3c6799bec76f7e319636032dd
Aakarsh Vinay <102870828+akarxxx1030@users.noreply.github.com>
2023-05-06 10:50:48 +0530

    Update alu_branch.sv

--- rtl/alu_branch.sv
+++ rtl/alu_branch.sv
@@ -1,23 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06.05.2023 10:36:21
-// Design Name: 
-// Module Name: alu_branch
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 module alu_branch(pccurrent,pcoffset,updatedpc);
 input logic [63:0]pccurrent;
 input logic [63:0]pcoffset;
@@ -25,7 +5,3 @@ output logic [63:0]updatedpc;
 assign updatedpc = pccurrent + pcoffset;
 endmodule
 
-module alu_branch(
-
-    );
-endmodule
