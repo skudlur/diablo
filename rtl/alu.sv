04eb1006956d9872e28354bd46604140c877591c
Suhas KV <suhaskv0111@gmail.com>
2023-05-07 11:23:28 +0530

    Cleanup: alu.sv cleaned up according to coderule

--- rtl/alu.sv
+++ rtl/alu.sv
@@ -2,25 +2,19 @@
 *	Arithmetic Logical Unit for Diablo     
 */
 
-module alu (
-	alu_input1,
-	alu_input2,
-	alu_output,
-	signal
+module alu #parameter(XLEN=64) ( 
+  	input logic [XLEN-1:0] alu_input1, alu_input2,
+  	input logic [1:0] signal,                       //More to be added  
+  	output logic [XLEN-1:0] alu_output
 );
-  
-  input logic [63:0] alu_input1, alu_input2;
-  input logic [1:0] signal;                       //More to be added  
-  output logic [63:0] alu_output;
-    
-  always_comb 
-  begin  
-    case(signal)
-    	2'b00: alu_output = alu_input1 + alu_input2; 
-    	2'b01: alu_output = alu_input1 - alu_input2;
-    	2'b10: alu_output = alu_input1 * alu_input2;
-    	2'b11: alu_output = alu_input1 / alu_input2;
-    	default: alu_output = 64'b0;
-    endcase 
-  end
+  	always_comb 
+  	begin  
+    		case(signal)
+    			2'b00: alu_output = alu_input1 + alu_input2; 
+		    	2'b01: alu_output = alu_input1 - alu_input2;
+		    	2'b10: alu_output = alu_input1 * alu_input2;
+		    	2'b11: alu_output = alu_input1 / alu_input2;
+		    	default: alu_output = 0;
+		endcase 
+  	end
 endmodule
