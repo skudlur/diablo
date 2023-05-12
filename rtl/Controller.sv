80acc975e6936816afe941cc0521da64dfcc143b
Aakarsh Vinay <102870828+akarxxx1030@users.noreply.github.com>
2023-05-12 21:53:03 +0530

    Update controller.sv

--- rtl/Controller.sv
+++ rtl/Controller.sv
@@ -1,54 +1,54 @@
-module Controller(
+module controller(
     
     //Input
-    input logic [6:0] Opcode, //7-bit opcode field from the instruction
+    input logic [6:0] opcode, //7-bit opcode field from the instruction
     
     //Outputs
-    output logic ALUSrc,//0: The second ALU operand comes from the second register file output (Read data 2); 
+    output logic aluSrc,//0: The second ALU operand comes from the second register file output (Read data 2); 
                   //1: The second ALU operand is the sign-extended, lower 16 bits of the instruction.
-    output logic MemtoReg,RegtoMem, //0: The value fed to the register Write data input comes from the ALU.
+    output logic memtoreg,regtomem, //0: The value fed to the register Write data input comes from the ALU.
                      //1: The value fed to the register Write data input comes from the data memory.
-    output logic RegWrite, //The register on the Write register input is written with the value on the Write data input 
-    output logic MemRead,  //Data memory contents designated by the address input are put on the Read data output
-    output logic MemWrite, //Data memory contents designated by the address input are replaced by the value on the Write data input.
-    output logic Branch,
-    output logic [1:0] ALUOp,
-    output logic Con_Jalr,Con_Jal,Mem,OpI,Con_AUIPC,Con_LUI
+    output logic regwrite, //The register on the Write register input is written with the value on the Write data input 
+    output logic memread,  //Data memory contents designated by the address input are put on the Read data output
+    output logic memwrite, //Data memory contents designated by the address input are replaced by the value on the Write data input.
+    output logic branch,
+    output logic [1:0] aluop,
+    output logic con_jalr,con_jal,mem,opi,con_auipc,con_lui
 
 );
 
-//    localparam R_TYPE = 7'b0110011;
-//    localparam LW     = 7'b0000011;
-//    localparam SW     = 7'b0100011;
-//    localparam BR     = 7'b1100011;
-//    localparam RTypeI = 7'b0010011; //addi,ori,andi
+//    localparam r_type = 7'b0110011;
+//    localparam lw     = 7'b0000011;
+//    localparam sw     = 7'b0100011;
+//    localparam br     = 7'b1100011;
+//    localparam rtypei = 7'b0010011; //addi,ori,andi
     
-    logic [6:0] R_TYPE, LW, SW, RTypeI, BR, JALR, JAL,AUIPC,LUI;
-    assign  BR     = 7'b1100011;
-    assign  R_TYPE = 7'b0110011;
-    assign  LW     = 7'b0000011;
-    assign  SW     = 7'b0100011;
-    assign  RTypeI = 7'b0010011; //addi,ori,andi
-    assign  JAL = 7'b1101111;
-    assign  JALR = 7'b1100111;
-    assign  AUIPC = 7'b0010111;
-    assign  LUI = 7'b0110111;
+    logic [6:0] r_type, lw, sw, rtypei, br, jalr, jal , auipc ,lui;
+    assign  br     = 7'b1100011;
+    assign  r_type = 7'b0110011;
+    assign  lw     = 7'b0000011;
+    assign  sw     = 7'b0100011;
+    assign  rtypei = 7'b0010011; //addi,ori,andi
+    assign  jal = 7'b1101111;
+    assign  jalr = 7'b1100111;
+    assign  auipc = 7'b0010111;
+    assign  lui = 7'b0110111;
 
-  assign Con_Jal = (Opcode == JAL);  
-  assign Con_Jalr = (Opcode == JALR);  
-  assign Branch = (Opcode == BR);  
-  assign ALUSrc   = (Opcode==LW || Opcode==SW || Opcode == RTypeI);
-  assign MemtoReg = (Opcode==LW);
-  assign RegtoMem = (Opcode==SW);
-  assign RegWrite = (Opcode==R_TYPE || Opcode==LW || Opcode == RTypeI || Opcode == JALR || Opcode == JAL);
-  assign Mem = (Opcode==LW||Opcode==SW);
-  assign MemRead  = (Opcode==LW);
-  assign MemWrite = (Opcode==SW||Opcode == JALR);
-  assign ALUOp[0] = (Opcode==BR);
-  assign OpI = (Opcode==RTypeI);
-  assign ALUOp[1] = (Opcode==R_TYPE);
-  assign Con_AUIPC = (Opcode==AUIPC);
-  assign Con_LUI = (Opcode==LUI);
+    assign con_jal = (opcode == jal);  
+    assign con_jalr = (opcode == jalr);  
+    assign branch = (opcode == br);  
+    assign alusrc   = (opcode==lw || opcode==sw || opcode == rtypei);
+    assign memtoreg = (opcode==lw);
+    assign regtomem = (opcode==sw);
+    assign regwrite = (opcode==r_type || opcode==lw || opcode == rtypei || opcode == jalr || opcode == jal);
+    assign mem = (opcode==lw||opcode==sw);
+    assign memread  = (opcode==lw);
+    assign memwrite = (opcode==sw||opcode == jalr);
+    assign aluop[0] = (opcode==br);
+    assign opi = (opcode==rtypei);
+    assign aluop[1] = (opcode==r_type);
+    assign con_auipc = (opcode==auipc);
+    assign con_lui = (opcode==lui);
   
 
 endmodule
