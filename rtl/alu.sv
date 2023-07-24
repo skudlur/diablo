136dbf2b128ffe5da6383b4c5bc5650b589c3390
Suhas KV <suhaskv0111@gmail.com>
2023-07-24 22:23:37 +0530

    Fixed: alu fixed for arithmetic, logical and shift operations and added defines file

--- rtl/alu.sv
+++ rtl/alu.sv
@@ -1,20 +1,37 @@
 /*
-*	Arithmetic Logical Unit for Diablo     
+*	Arithmetic Logical Unit for Diablo
 */
 
-module alu #(parameter XLEN = 64)( 
-  	input logic [XLEN-1:0] alu_input1, alu_input2,
-  	input logic [1:0] signal,                       //More to be added  
-  	output logic [XLEN-1:0] alu_output
+module alu (
+  input logic [31:0]  alu_input1, alu_input2,
+  input logic [6:0] alu_op,
+  output logic [31:0] alu_output
 );
-  	always_comb 
-  	begin  
-    		case(signal)
-    			2'b00: alu_output = alu_input1 + alu_input2; 
-		    	2'b01: alu_output = alu_input1 - alu_input2;
-		    	2'b10: alu_output = alu_input1 * alu_input2;
-		    	2'b11: alu_output = alu_input1 / alu_input2;
-		    	default: alu_output = 0;
-		endcase 
-  	end
+
+   `include "diablo_pkg.sv";
+
+   always_comb
+   begin
+     unique case(alu_op)
+       // Arithmetic operations
+
+       `ALU_ADD: alu_output = alu_input1 + alu_input2;                                        // Add instruction
+       `ALU_SUB: alu_output = alu_input1 - alu_input2;                                        // Sub instruction
+       `ALU_AND: alu_output = alu_input1 & alu_input2;                                        // AND instruction
+       `ALU_OR : alu_output = alu_input1 | alu_input2;                                        // OR instruction
+       `ALU_XOR: alu_output = alu_input1 ^ alu_input2;                                        // XOR instruction
+
+       // Shift operations
+
+       `ALU_SLL: alu_output = alu_input1 << (alu_input2 & 5'b11111);                           // Shift left logical instruction
+       `ALU_SRL: alu_output = alu_input1 >> (alu_input2 & 5'b11111);                           // Shift right logical instruction
+       `ALU_SRA: alu_output = $signed(alu_input1) >> (alu_input2 & 5'b11111);                  // Shift right arithmetic instruction
+
+       // Logical operations
+
+       `ALU_SLT: alu_output = ($signed(alu_input1) < $signed(alu_input2)) ? 32'b1 : 32'b0;    // Set if less than
+       `ALU_SLTU: alu_output = (alu_input1 < alu_input2) ? 32'b1 : 32'b0;                     // Set if less than unsigned
+       default: alu_output = 0;
+     endcase
+   end
 endmodule
---
+++ rtl/diablo_defines.sv
@@ -0,0 +1,16 @@
+/*
+*   Package for diablo
+*/
+
+`define ALU_ADD 	7'b000_0000
+`define ALU_SUB 	7'b000_0001
+`define ALU_AND 	7'b000_0010
+`define ALU_OR 		7'b000_0011
+`define ALU_XOR 	7'b000_0100
+
+`define ALU_SLL 	7'b000_0101
+`define ALU_SRL 	7'b000_0110
+`define ALU_SRA 	7'b000_0111
+
+`define ALU_SLT 	7'b000_1000
+`define ALU_SLTU 	7'b000_1001
